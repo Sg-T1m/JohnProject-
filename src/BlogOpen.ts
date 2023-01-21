7faacc4422e620089c15053da0663dc6f1e2c79d
Sg-T1m <perevozchikov.t.a@gmail.com>
2023-01-21 23:43:56 +0300

    отрисовка контента для блога

--- src/BlogOpen.ts
+++ src/BlogOpen.ts
@@ -1,7 +1,7 @@
 import { FectInquiry } from "./FetchInquiry";
-const dvdvds: string = `http://localhost:3000/news`
+const dvdvds: string = `http://localhost:3000/Blog`
 const ddds: string = `svsdv`
-const modalNav2 = new FectInquiry(ddds, dvdvds, renderBlogPage)
+const modalNav2 = new FectInquiry(ddds, dvdvds, renderBlogContent)
 const openBlogs: Element | null = document.querySelector(`.hrev_Blog`)
 const Home_Listners: any = document.querySelector(".content");
 
@@ -16,22 +16,34 @@ function renderBlogPage(): void {
     <p class="Blog_headers_text">Blog </p>
   </div>
   <div class="Blog_content">
-    <div class="Blog_content_Container">
-      <div class="content_Container_heaer">UI Interactions of the week</div>
-      <div class="content_Container_middle">
-        <div class="Container_middle_time">12 Feb 2019</div>
-        <div class="Container_middle_line"></div>
-        <div class="Container_middle_text">Express, Handlebars</div>
-      </div>
-      <div class="content_Container_Footer"><p class="Container_Footer_text">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-        sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
-      </div>
-    </div>
   </div>
 </div>`
     Home_Listners.innerHTML = "";
     Home_Listners.appendChild(BlogHtml);
+    modalNav2.gets()
 }
-function renderBlogContent(data:string):void{
-
+interface items {
+    Headertext: string;
+    date: string;
+    theme: string;
+    content: string;
+}
+function renderBlogContent(data: Array<items>): void {
+    const inerContent: Element | null = document.querySelector(`.Blog_content`)
+    data.forEach((item: items): void => {
+        let Blog_contentRender = document.createElement("div");
+        Blog_contentRender.classList.add("Blog_content");
+        Blog_contentRender.innerHTML = `
+        <div class="Blog_content_Container">
+        <div class="content_Container_heaer">${item.Headertext}</div>
+        <div class="content_Container_middle">
+          <div class="Container_middle_time">${item.date}</div>
+          <div class="Container_middle_line"></div>
+          <div class="Container_middle_text">${item.theme}</div>
+        </div>
+        <div class="content_Container_Footer"><p class="Container_Footer_text">${item.content}</p>
+        </div>
+                `;
+        inerContent?.appendChild(Blog_contentRender);
+    });
 }
